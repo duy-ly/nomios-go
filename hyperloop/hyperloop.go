dec2bd542c0b35e3d1b406b3ea08b31a4c076207
wizk3y <hoangvu7939@gmail.com>
2023-04-26 19:27:18 +0700

    WIP: add event stream to hyperloop

--- hyperloop/hyperloop.go
+++ hyperloop/hyperloop.go
@@ -1,7 +1,20 @@
 package hyperloop
 
+import (
+	"github.com/duy-ly/nomios-go/event"
+)
+
 type Hyperloop struct {
+	stream chan event.NomiosEvent
+
 	running bool
+}
+
+func NewHyperloop() *Hyperloop {
+	h := new(Hyperloop)
+	h.stream = make(chan event.NomiosEvent, 20000)
+
+	return h
 }
 
 func (h *Hyperloop) Start() {
